48d8fd08517cbcc79ce41c2784f0c9f5fc278681
Howard Chu <howardchu95@gmail.com>
2025-09-30 04:19:35 +0000

    Remove tag line

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,6 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Howard Chu\'s Homepage',
-  tagline: 'To eliminate all bugs',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
